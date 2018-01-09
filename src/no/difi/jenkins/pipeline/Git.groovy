84d71941a4c5e1acec1a4b01dcdd9af8b1248b06
Thomas Johansen <thxmasj@gmail.com>
2018-01-09 11:09:50 +0000

    2018-01-09-1109-17fee55|PBLEID-15483: Prepare verification-stadiet feiler dersom det kjører første gang på eid-jenkins03.dmz.local

--- src/no/difi/jenkins/pipeline/Git.groovy
+++ src/no/difi/jenkins/pipeline/Git.groovy
@@ -8,6 +8,11 @@ String readCommitMessage() {
     sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
 }
 
+String repositoryName() {
+    String repositoryUrl = sh returnStdout: true, script: 'git remote get-url origin'
+    repositoryUrl.tokenize(':/')[-1].tokenize('.')[0].trim()
+}
+
 void waitForAvailableVerificationSlot(def sshKey) {
     sshagent([sshKey]) {
         while (true) {
@@ -23,6 +28,48 @@ void waitForAvailableVerificationSlot(def sshKey) {
                 return
             }
         }
+    }
+}
+
+void createVerificationBranch(String logEntry, String sshKey) {
+    sshagent([sshKey]) {
+        return sh(returnStdout: true, script: """
+        #!/usr/bin/env bash
+    
+        __fail() {
+            local ret=\$?
+            >&2 printf "\u274c \$1\n"
+            return \${ret}
+        }
+    
+        __masterBranch() {
+            echo \${MASTER_BRANCH:-'master'}
+        }
+    
+        createRevision() {
+            git fetch origin \$(__masterBranch) >&2 || { __fail "Failed to update remote tracking master branch"; return 1; }
+            git branch --contains origin/\$(__masterBranch) | grep ${BRANCH_NAME} > /dev/null \
+                || { >&2 echo "Current branch does not contain entire master branch. Implicit merge will be performed"; }
+            local author=\$(git --no-pager show -s --format='%an <%ae>' ${BRANCH_NAME}) || { __fail "Failed to extract author"; return 1; }
+            local tmpBranch=\$(date +%s | sha256sum | base64 | head -c 32) || { __fail "Failed to generate UUID for temporary branch name"; return 1; }
+            git checkout -b \${tmpBranch} origin/\$(__masterBranch) >/dev/null || { __fail "Failed to create verification branch"; return 1; }
+            git config --local user.name 'Jenkins'
+            git config --local user.email 'jenkins@difi.no'
+            git merge --squash ${BRANCH_NAME} >/dev/null || { __fail "Failed to add work to verification branch"; return 1; }
+            output=\$(git commit --allow-empty --author "\${author}" -am "${logEntry}") || { __fail "Failed to commit verification branch: \${output}"; return 1; }
+            git config --local --unset user.name
+            git config --local --unset user.email
+            local commitId
+            commitId=\$(git rev-parse HEAD) || { __fail "Failed to find commit id"; }
+            git checkout ${BRANCH_NAME} >/dev/null || { __fail "Failed to return to work branch"; return 1; }
+            git branch -D \${tmpBranch} >/dev/null || __fail "Failed to delete local temporary branch (run 'git branch -D \${tmpBranch}' to fix it)"
+            local output
+            output=\$(git push -f -u origin \${commitId}:refs/heads/verify/${BRANCH_NAME} 2>&1) || { __fail "Failed to push tagged verification revision: \${output}"; return 1; }
+            echo -n \${commitId}
+        }
+        
+        createRevision
+        """)
     }
 }
 
--- vars/prepareVerification.groovy
+++ vars/prepareVerification.groovy
@@ -5,13 +5,13 @@ import no.difi.jenkins.pipeline.Crucible
 import static java.time.ZonedDateTime.now
 
 def call(String gitSshKey, String crucibleUrl, String crucibleProjectKey, String crucibleUser, String cruciblePassword) {
-    env.version = DateTimeFormatter.ofPattern('yyyy-MM-dd-HHmm').format(now(ZoneId.of('UTC'))) + "-" + new Git().readCommitId()
+    Git git = new Git()
+    env.version = DateTimeFormatter.ofPattern('yyyy-MM-dd-HHmm').format(now(ZoneId.of('UTC'))) + "-" + git.readCommitId()
     String issueSummary = issueSummary()
     String issueId = issueId()
     String commitMessage = "${env.version}|${issueId}: ${issueSummary}".toString()
-    String commitId = createVerificationRevision commitMessage, gitSshKey
-    String repositoryUrl = 'git remote get-url origin'.execute([], new File(pwd())).text
-    String repositoryName = repositoryUrl.tokenize(':/')[-1].tokenize('.')[0].trim()
+    String commitId = git.createVerificationBranch commitMessage, gitSshKey
+    String repositoryName = git.repositoryName()
     Crucible crucible = new Crucible()
     crucible.synchronize crucibleUrl, repositoryName, crucibleUser, cruciblePassword
     crucible.createReview commitId, issueSummary, issueId, crucibleUrl, repositoryName, crucibleProjectKey, crucibleUser, cruciblePassword
