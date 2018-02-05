6dc8103ee15ffc90a9287bd40171be0b3d780224
Thomas Johansen <thxmasj@gmail.com>
2018-02-05 11:01:04 +0100

    wip

--- src/no/difi/jenkins/pipeline/Maven.groovy
+++ src/no/difi/jenkins/pipeline/Maven.groovy
@@ -8,7 +8,7 @@ Docker docker
 void verify(def options) {
     String settingsFile = settingsFile()
     env.MAVEN_OPTS = options ?: ""
-    sh "mvn clean verify -B -s ${settingsFile}"
+    sh "unset JENKINS_HOME; mvn clean verify -B -s ${settingsFile}"
     sh "rm ${settingsFile}"
 }
 
